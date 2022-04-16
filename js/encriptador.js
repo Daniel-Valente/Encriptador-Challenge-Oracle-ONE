99743036cb07f4554ce12162e119d973c9979534
Daniel-Valente <valente.gar.daniel@gmail.com>
2022-04-16 13:38:10 -0500

    Establecer logo y pagina responsive

---
+++ js/encriptador.js
@@ -0,0 +1,34 @@
+var btn_encriptar = document.querySelector("#encriptar");
+
+btn_encriptar.addEventListener("click", function() { 
+    var sentence = document.querySelector("#sentence").value;
+    var final_sentence = "";
+
+    if(sentence.length) {
+        for(var i = 0; i < sentence.length; i++) {
+            var char = sentence.charAt(i);
+            switch(char) {
+                case 'a':
+                    final_sentence += "ai";
+                    break;
+                case 'e':
+                    final_sentence += "enter";
+                    break;
+                case 'i':
+                    final_sentence += "imes";
+                    break;
+                case 'o':
+                    final_sentence += "ober";
+                    break;
+                case 'u':
+                    final_sentence += "ufat";
+                    break;
+                default:
+                    final_sentence += char;
+                    break;
+            }
+        }
+    }
+    document.querySelector("#sentence").value = "";
+    console.log(final_sentence);
+});
